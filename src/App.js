07791448042a8a828b866a5247c2be3f094625bb
nooberonie1323 <htcorraya@gmail.com>
2025-02-06 22:35:28 +0600

    Initial commit: Add project files

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className="hero">
+        <h1>Welcome to the Songwriter's Page</h1>
+        <p>Your journey to creativity starts here!</p>
+        <button>Get Started</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
